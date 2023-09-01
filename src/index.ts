a62c48da277e3fd8da5c913883c271bf75fc2271
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-09-01 12:33:10 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-import fs from 'node:fs/promises';
+import * as fs from 'node:fs'; // Import fs for both sync and async methods
 import path from 'node:path';
 import { createVideo, uploadVideo } from './helpers';
 
@@ -16,61 +16,63 @@ const title = argv.title as string;
 
 async function main() {
     try {
-        
         if (!file) {
             throw new Error('Please specify a file');
         }
-        
+
         if (!key || !/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}-[a-z0-9]{4}-[a-z0-9]{4}/.test(key)) {
             throw new Error('Please specify a valid key');
         }
-        
+
         if (!library || Number.isNaN(Number(library))) {
             throw new Error('Please specify a valid library id');
         }
-        
+
         if (!collection || !/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/.test(collection)) {
             throw new Error('Please specify a valid collection id');
         }
-        
+
         if (!title) {
             throw new Error('Please specify a title');
         }
-        
+
         const filePath = path.resolve(process.cwd(), file);
-        
+
         try {
-          await fs.access(filePath); // Check if the file exists
+            await fs.promises.access(filePath); // Check if the file exists
         } catch (e) {
-          throw new Error('File does not exist');
+            throw new Error('File does not exist');
         }
 
         console.log(`Reading file: ${filePath}`);
         const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
         let content = '';
-        
-        readStream.on('data', (chunk) => {
-          content += chunk;
+
+        readStream.on('data', (chunk: string) => {
+            content += chunk;
         });
-        
+
         readStream.on('end', async () => {
+            let uploadResult: any; // Declare uploadResult and guid before the callback
+            let guid: string;
+
             const createResult = await createVideo(Number(library), title, collection, key);
-        
+
             if (!createResult.success) {
                 throw new Error('Could not create video');
             }
-        
-            const guid = createResult.data;
-        
-            const uploadResult = await uploadVideo(Number(library), guid, key, Buffer.from(content, 'utf8'););
-        
+
+            guid = createResult.data;
+
+            uploadResult = await uploadVideo(Number(library), guid, key, Buffer.from(content, 'utf8'));
+
             if (!uploadResult.success) {
                 throw new Error('Could not upload video');
             }
-        
+
             console.log(`Video uploaded: ${guid}`);
         });
-    } catch(e) {
+    } catch (e) {
         console.log(e);
     }
 }
